73cac3fbd04a1e6303762d93da6287ca20b5f802
Jonathan Redford <jnredford@gmail.com>
2026-04-14 10:50:10 -0500

    Add 150ms initial video buffer for smoother playback

--- HaishinKit/Sources/Stream/MediaLink.swift
+++ HaishinKit/Sources/Stream/MediaLink.swift
@@ -2,8 +2,8 @@ import CoreMedia
 import Foundation
 
 final actor MediaLink {
-    static let capacity = 90
-    static let duration: TimeInterval = 0.0
+    static let capacity = 120
+    static let duration: TimeInterval = 0.15  // 150ms initial buffer for smoother playback
 
     var dequeue: AsyncStream<CMSampleBuffer> {
         AsyncStream { continutation in
